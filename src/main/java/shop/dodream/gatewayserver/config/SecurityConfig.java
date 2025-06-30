c0168dafdd9d06a1a19fc3257b87560d42f02c02
HyunJin <guswlsdl7771@naver.com>
2025-06-30 14:42:05 +0900

    merge: feat/22-add-security to develop (#23)
    - security를 추가함으로 필터 역할 축소
    - security에서 인가처리
    
    #22

---
+++ src/main/java/shop/dodream/gatewayserver/config/SecurityConfig.java
@@ -0,0 +1,31 @@
+package shop.dodream.gatewayserver.config;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
+import org.springframework.security.config.web.server.SecurityWebFiltersOrder;
+import org.springframework.security.config.web.server.ServerHttpSecurity;
+import org.springframework.security.web.server.SecurityWebFilterChain;
+import shop.dodream.gatewayserver.jwt.JwtAuthenticationFilter;
+import shop.dodream.gatewayserver.jwt.JwtTokenProvider;
+
+@Configuration
+@EnableWebFluxSecurity
+@RequiredArgsConstructor
+public class SecurityConfig {
+    private final JwtTokenProvider jwtTokenProvider;
+
+    @Bean
+    public SecurityWebFilterChain springSecurityFilterChain(ServerHttpSecurity http) {
+        return http
+                .csrf(ServerHttpSecurity.CsrfSpec::disable)
+                .authorizeExchange(exchanges-> exchanges
+                        .pathMatchers("/auth/**","/actuator/**","/users/signup/**").permitAll()
+                        .pathMatchers("/admin/**").hasRole("ADMIN")
+                        .anyExchange().authenticated()
+                )
+                .addFilterAt(new JwtAuthenticationFilter(jwtTokenProvider), SecurityWebFiltersOrder.AUTHENTICATION)
+                .build();
+    }
+}
